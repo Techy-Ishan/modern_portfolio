c48053949f8d08aa1f986236dc96c6c3ea132bfe
Ishan chudali <paudyalishan360@gmail.com>
2022-05-18 12:33:20 +0545

    Added media queries and overlay responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,6 +17,7 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
+
     // set menu state
     showMenu = true;
   } else {
